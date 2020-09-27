e99a7957c715fe42e690ddbce762877151991e1d
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-27 14:36:28 -0400

    Adds some lines to urls, settings, and adds cor handling in settings

--- class_backend/class_backend/settings.py
+++ class_backend/class_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders', 
     'class',
 ]
 
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+     'https://localhost:3000/'
+ )
